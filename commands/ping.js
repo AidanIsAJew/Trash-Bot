e4a3f776dbdc6fb4c5f5241cbf33b92eae068930
Aidan <38870143+AidanIsAJew@users.noreply.github.com>
2018-09-03 20:51:55 -0400

    2018-09-03_0851 PM

---
+++ commands/ping.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+// Create an instance of a Discord client
+const client = new Discord.Client();
+// I dont remeber what this does \_O-O_/
+const fs = require("fs")
+// Config
+const config = require("./settings/config.json");
+// Defualt prefix
+const defPrefix = "!";
+
+
+module.exports = {
+    run: async (message) => {
+
+    }
+}
