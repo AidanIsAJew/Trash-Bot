0047d40732789283251a64bc6fc3be49a1f92128
Aidan <38870143+AidanIsAJew@users.noreply.github.com>
2018-10-06 19:20:25 -0400

    Delete pull-push-git.bat

--- pull-push-git.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-git pull origin 2.0
-git add .
-git commit -m "%mydate%_%mytime%"
-git push -u origin 2.0
-pause
