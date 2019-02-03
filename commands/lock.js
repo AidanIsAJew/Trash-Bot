703b7eecba1a63fd675591057b601247a862590d
AidanWright <shinqbusiness@gmail.com>
2019-02-02 19:56:29 -0500

    2019-02-02_0756 PM

--- commands/lock.js
+++ commands/lock.js
@@ -1,5 +1,12 @@
 exports.run = async (client, message, args, level) => {
-    const role = message.guild.defaultRole;
+    const role;
+    const settings = client.getSettings(message.guild.id);
+    if (settings.reactToJoin == "true") {
+      role = message.guild.roles.find(x => x.name == "member");
+    }
+    else {
+      role = message.guild.defaultRole;
+    }
     console.log(role);
     if (!role) return client.logger.error(`no defualt role found`)
     message.channel.overwritePermissions(role, { //first you pass the id OR the member OR the user OR a role
--- commands/unlock.js
+++ commands/unlock.js
@@ -1,6 +1,13 @@
 exports.run = async (client, message, args, level) => {
-    const role = message.guild.defaultRole;
-    console.log(role);
+    const role;
+    const settings = client.getSettings(message.guild.id);
+    if (settings.reactToJoin == "true") {
+      role = message.guild.roles.find(x => x.name == "member");
+    }
+    else {
+      role = message.guild.defaultRole;
+    }
+      console.log(role);
     if (!role) return client.logger.error(`no defualt role found`)
     message.channel.overwritePermissions(role, { //first you pass the id OR the member OR the user OR a role
         SEND_MESSAGES: true,
